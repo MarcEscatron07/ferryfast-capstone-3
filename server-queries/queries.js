5385aa77c6ae4850251c9b5b226c72b685b127db
Marc Benedict Escatron <marc.escatron07@gmail.com>
2020-01-30 15:18:10 +0800

    setup server-queries.js for Origins and Destinations collections

--- server-queries/queries.js
+++ server-queries/queries.js
@@ -4,6 +4,9 @@ const Role = require('../models/Role');
 const Stat = require('../models/Stat');
 const Administrator = require('../models/Administrator');
 
+const Origin = require('../models/Origin');
+const Destination = require('../models/Destination');
+
 const typeDefs = gql`
     type RoleType {
         id: ID
@@ -30,29 +33,41 @@ const typeDefs = gql`
         stat: StatType
     }
 
+    type OriginType {
+        id: ID
+        name: String
+        destinations: [DestinationType]
+    }
+    type DestinationType {
+        id: ID
+        name: String
+        originId: String
+        origin: OriginType
+    }
+
     type Query {
         getRoles(id: String): [RoleType]
         getRole(id: String): RoleType
-
         getStats(id: String): [StatType]
         getStat(id: String): StatType
-
         getAdministrators(id: String): [AdministratorType]
         getAdministrator(id: String): AdministratorType
-    }
 
+        getOrigins(id: String): [OriginType]
+        getOrigin(id: String): OriginType
+        getDestinations(id: String): [DestinationType]
+        getDestination(id: String): DestinationType
+    }
     type Mutation {
         createRole(
             uniqueId: Int
             name: String
         ): RoleType
-        
         updateRole(
             id: ID
             uniqueId: Int
             name: String
         ): RoleType
-
         deleteRole(
             id: ID
         ): RoleType
@@ -61,13 +76,11 @@ const typeDefs = gql`
             uniqueId: Int
             name: String
         ): StatType
-        
         updateStat(
             id: ID
             uniqueId: Int
             name: String
         ): StatType
-
         deleteStat(
             id: ID
         ): StatType
@@ -81,7 +94,6 @@ const typeDefs = gql`
             roleId: Int
             statId: Int
         ): AdministratorType
-
         updateAdministrator(
             id: ID
             username: String
@@ -92,10 +104,23 @@ const typeDefs = gql`
             roleId: Int
             statId: Int
         ): AdministratorType
-
         deleteAdministrator(
             id: ID
         ): AdministratorType
+
+        createOrigin(
+            name: String
+            originId: String
+        ): OriginType
+        udpateOrigin(
+            id: ID
+            name: String
+            originId: String
+        ): OriginType
+        deleteOrigin(
+            id: ID
+        ): OriginType
+
     }
 `;
 
@@ -124,26 +149,56 @@ const resolvers = {
         },
         getAdministrator: (_,args) => {
             return Administrator.findById(args.id);
+        },
+
+        getOrigins: (_,args) => {
+            if(!args.id)
+                return Origin.find({});
+            return Origin.find({_id:args.id});
+        },
+        getOrigin: (_,args) => {
+            return Origin.findById(args.id);
+        },
+        getDestinations: (_,args) => {
+            if(!args.id)
+                return Destination.find({});
+            return Destination.find({_id:args.id});
+        },
+        getDestination: (_,args) => {
+            return Destination.findById(args.id);
         }
     },
+
     RoleType: {
         administrators: (parent,args) => {
-            return Administrator.find({roleId: parent.uniqueId});
+            return Administrator.find({roleId:parent.uniqueId});
         }
     },
     StatType: {
         administrators: (parent,args) => {
-            return Administrator.find({statId: parent.uniqueId});
+            return Administrator.find({statId:parent.uniqueId});
         }
     },
     AdministratorType: {
         role: (parent,args) => {
-            return Role.findOne({uniqueId: parent.roleId});
+            return Role.findOne({uniqueId:parent.roleId});
         },
         stat: (parent,args) => {
-            return Stat.findOne({uniqueId: parent.statId})
+            return Stat.findOne({uniqueId:parent.statId});
         }
     },
+
+    OriginType: {
+        destinations: (parent,args) => {
+            return Destination.find({originId:parent.id});
+        }
+    },
+    DestinationType: {
+        origin: (parent,args) => {
+            return Origin.findOne({_id:parent.originId});
+        }
+    },
+
     Mutation: {
         createRole: (_,args) => {
             let newRole = Role({
@@ -208,6 +263,42 @@ const resolvers = {
         },
         deleteAdministrator: (_,args) => {
             return Administrator.findOneAndDelete({_id:args.id});
+        },
+
+        createOrigin: (_,args) => {
+            let newOrigin = Origin({
+                name: args.name
+            })
+            return newOrigin.save();
+        },
+        updateOrigin: (_,args) => {
+            let updateOriginId = {_id:args.id}
+            let updateOriginData = {
+                name: args.name
+            }
+            return Origin.findOneAndUpdate(updateOriginId, updateOriginData);
+        },
+        deleteOrigin: (_,args) => {
+            return Origin.findOneAndDelete({_id:args.id});
+        },
+
+        createDestination: (_,args) => {
+            let newDestination = Destination({
+                name: args.name,
+                originId: args.originId
+            })
+            return newDestination.save();
+        },
+        updateDestination: (_,args) => {
+            let updateDestinationId = {_id:args.id}
+            let updateDestinationData = {
+                name: args.name,
+                originId: args.originId
+            }
+            return Destination.findOneAndUpdate(updateDestinationId, updateDestinationData);
+        },
+        deleteDestination: (_,args) => {
+            return Destination.findOneAndDelete({_id:args.id});
         }
     }
 }
